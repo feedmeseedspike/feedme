2ffbdef568a7d1e308dc6fd84956ac779769f4e8
tolu2507 <tolusnehhil@gmail.com>
2025-08-11 22:31:49 +0100

    fixed

--- src/app/api/bundles/route.ts
+++ src/app/api/bundles/route.ts
@@ -5,7 +5,6 @@ export async function GET(req: Request) {
   try {
     const products = await getBundle();
     // Only return id, slug, name, image
-    console.log(products);
     return NextResponse.json({ products });
   } catch (error) {
     return NextResponse.json(
